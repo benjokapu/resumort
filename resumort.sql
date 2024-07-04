138e3c552fb9e847b47c534883fa7a91e829cedb
benjokapu <144476747+benjokapu@users.noreply.github.com>
2024-07-04 18:25:11 -0300

    update on db

--- resumort.sql
+++ resumort.sql
@@ -1,164 +1,34 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 04-07-2024 a las 17:53:00
--- Versión del servidor: 5.7.17-log
--- Versión de PHP: 5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `resumort`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `especialidad`
---
-
-CREATE TABLE `especialidad` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `especialidad`
---
-
-INSERT INTO `especialidad` (`id`, `nombre`) VALUES
-(1, 'tic'),
-(2, 'gestion'),
-(3, 'medios'),
-(4, 'diseño');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `subjectsavanzado`
---
-
-CREATE TABLE `subjectsavanzado` (
-  `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `subjectsavanzado`
---
-
-INSERT INTO `subjectsavanzado` (`nombre`) VALUES
-('cultura judia'),
-('educacion fisica'),
-('educacion judia'),
-('etica'),
-('fisica'),
-('geografia'),
-('historia'),
-('lengua'),
-('matematica');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `subjectsbasico`
---
-
-CREATE TABLE `subjectsbasico` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `subjectsbasico`
---
-
-INSERT INTO `subjectsbasico` (`id`, `nombre`) VALUES
-(1, 'arte'),
-(2, 'biologia'),
-(3, 'educacion fisica'),
-(4, 'educacion judia'),
-(5, 'educacion tecnologica'),
-(6, 'etica'),
-(7, 'geografia'),
-(8, 'hebreo'),
-(9, 'historia'),
-(10, 'ingles'),
-(11, 'lengua'),
-(12, 'matematica');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
 CREATE TABLE `users` (
-  `dni` int(11) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+   `dni` INT AUTO_INCREMENT,
+   `password` VARCHAR(255),
+   `nombre` VARCHAR(255),
+   `apellido` VARCHAR(255),
+   `mail` VARCHAR(255),
+   `admin` BIT,
+   PRIMARY KEY (`dni`)
+);
 
--- --------------------------------------------------------
+CREATE TABLE `files` (
+   `id` INT AUTO_INCREMENT,
+   `nombre` VARCHAR(255),
+   `aclaraciones` TEXT,
+   `fecha` DATETIME,
+   `users_dni` INT,
+   `subjectYearId` VARCHAR(255),
+   `especialidad_id` INT,
+   `status` BIT,
+   PRIMARY KEY (`id`)
+);
 
---
--- Estructura de tabla para la tabla `years`
---
+CREATE TABLE `subjectsYear` (
+   `id` INT AUTO_INCREMENT,
+   `year` INT,
+   `especialidad` VARCHAR(255),
+   `nombre` VARCHAR(255),
+   PRIMARY KEY (`id`)
+);
 
-CREATE TABLE `years` (
-  `year` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Índices para tablas volcadas
---
+ALTER TABLE `files` ADD CONSTRAINT `FK_81f0a4a9-5f52-4970-a2b3-2170ae35a665` FOREIGN KEY (`users_dni`) REFERENCES `users`(`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Indices de la tabla `especialidad`
---
-ALTER TABLE `especialidad`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `subjectsbasico`
---
-ALTER TABLE `subjectsbasico`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`dni`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `especialidad`
---
-ALTER TABLE `especialidad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `subjectsbasico`
---
-ALTER TABLE `subjectsbasico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `dni` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `files` ADD CONSTRAINT `FK_1a83c299-ef1a-4281-8e7b-1328c2e75ccd` FOREIGN KEY (`subjectYearId`) REFERENCES `subjectsYear`(`id`)  ;
